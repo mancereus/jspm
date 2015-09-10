590c12c15208cb851cdec9c967fc4bff76611676
Matthias Schmitt <matthias.schmitt@scoop-software.de>
2015-09-10 13:36:18 +0200

    page-break

---
+++ nd_server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var os = require('os');
+
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n' + os.hostname());
+}).listen(8124);
+
+console.log('Server running at http://127.0.0.1:8124/');
