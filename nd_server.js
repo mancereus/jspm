d884de9fc71c19863346780d75d128c0b3e3754a
Matthias Schmitt <matthias.schmitt@scoop-software.de>
2015-11-23 11:28:56 +0100

    refactor

--- nd_server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var os = require('os');
-
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n' + os.hostname());
-}).listen(8124);
-
-console.log('Server running at http://127.0.0.1:8124/');
