1d436cb58f1171aca774816cd955cd5a5c6a88e4
Matthias Schmitt <matthias.schmitt@scoop-software.de>
2015-12-14 16:05:40 +0100

    cards

---
+++ app/test/polymer/data2.js
@@ -0,0 +1,9 @@
+var cards = {
+  stack: [	{key: 'wK', name: 'white king'},
+    {key: 'wK', name: 'white queen'},
+    {key: 'wK', name: 'white rogue'},
+    {key: 'wK', name: 'white pawn'},
+    {key: 'wK', name: 'white pawn'},
+	],
+  player1: [{key: 'c'}, {key: 'd2'}]
+}
