ef408e649006699c16c13131cc5b2d9adf32ead0
Matthias Schmitt <matthias.schmitt@scoop-software.de>
2016-01-11 14:28:25 +0100

    dice

---
+++ app/hotdice/data2.js
@@ -0,0 +1,11 @@
+var cards = {
+  stack: [	{key: 'wK', name: 'white king'},
+    {key: 'wQ', name: 'white queen'},
+    {key: 'wR', name: 'white rogue'},
+    {key: 'wP', name: 'white pawn'},
+    {key: 'wP', name: 'white pawn'},
+	],
+  player1: [{key: 'c'}, {key: 'd2'}],
+  player2: [{key: 'z'}],
+  player3: [{key: 'y'}],
+}
