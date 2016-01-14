f88c16e451f52851cd08effc1dc2a51f7430d447
Matthias Schmitt <matthias.schmitt@scoop-software.de>
2016-01-14 09:40:21 +0100

    bgfx

--- app/hotdice/data2.js
+++ app/hotdice/data2.js
@@ -13,7 +13,7 @@ var cards = {
         { color: 'orange', items: [3, 4,4, 5,5,'X'],val: "z"  },
         { color: 'orange', items: [3, 4,4, 5,5,'X'],val: "z"  },
     ],
-    player1: [
+    roll: [
         { color: 'lightblue', items: [1,1,2,2,3,4],val: "z"  },
         { color: 'lightgreen', items: [2, 3, 4, 5],val: "z"  },
         { color: 'lightyellow', items: [3,3, 4,4, 5, 'X'],val: "z"  },
--- app/test/polymer/data2.js
+++ app/test/polymer/data2.js
@@ -9,3 +9,27 @@ var cards = {
   player2: [{key: 'z'}],
   player3: [{key: 'y'}],
 }
+var cards = {
+    stack: [
+        { color: 'lightblue', items: [1,1,2,2,3,4],val: "z" },
+        { color: 'lightblue', items: [1,1,2,2,3,4],val: "z"  },
+        { color: 'lightblue', items: [1,1,2,2,3,4],val: "z"  },
+        { color: 'lightgreen', items: [2,2, 3,3, 4, 5],val: "z"  },
+        { color: 'lightgreen', items: [2,2, 3,3, 4, 5],val: "z"  },
+        { color: 'lightgreen', items: [2,2, 3,3, 4, 5],val: "z"  },
+        { color: 'lightyellow', items: [3,3, 4,4, 5, 'X'],val: "z"  },
+        { color: 'lightyellow', items: [3,3, 4,4, 5, 'X'],val: "z"  },
+        { color: 'lightyellow', items: [3,3, 4,4, 5, 'X'],val: "z"  },
+        { color: 'orange', items: [3, 4,4, 5,5,'X'],val: "z"  },
+        { color: 'orange', items: [3, 4,4, 5,5,'X'],val: "z"  },
+        { color: 'orange', items: [3, 4,4, 5,5,'X'],val: "z"  },
+    ],
+    player1: [
+        { color: 'lightblue', items: [1,1,2,2,3,4],val: "z"  },
+        { color: 'lightgreen', items: [2, 3, 4, 5],val: "z"  },
+        { color: 'lightyellow', items: [3,3, 4,4, 5, 'X'],val: "z"  },
+        { color: 'orange', items: [3, 4,4, 5,5,'X'],val: "z"  },
+        ],
+    player2: [],
+    player3: [],
+}
