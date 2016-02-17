5918dd7384ceb13d18688909786d38875b73596a
Matthias Schmitt <matthias.schmitt@scoop-software.de>
2016-02-17 16:20:34 +0100

    serviette

---
+++ app/servietten/data.js
@@ -0,0 +1,31 @@
+var cards = [
+    {name: "B", color: "orange"},
+    {name: "C", color: "yellow"},
+    {name: "A", color: "green"},
+    {name: "D", color: "red"},
+    {name: "E", color: "blue"},
+
+    {name: "D", color: "red"},
+    {name: "E", color: "blue"},
+    {name: "B", color: "orange"},
+    {name: "A", color: "green"},
+    {name: "C", color: "yellow"},
+
+    {name: "C", color: "yellow"},
+    {name: "A", color: "green"},
+    {name: "D", color: "red"},
+    {name: "B", color: "orange"},
+    {name: "E", color: "blue"},
+
+    {name: "B", color: "orange"},
+    {name: "E", color: "blue"},
+    {name: "C", color: "yellow"},
+    {name: "A", color: "green"},
+    {name: "D", color: "red"},
+
+    {name: "D", color: "red"},
+    {name: "C", color: "yellow"},
+    {name: "A", color: "green"},
+    {name: "B", color: "orange"},
+    {name: "E", color: "blue"},
+]
