4f683e62ef79f9310d454aaca49c9352e7b13ed6
Matthias Schmitt <matthias.schmitt@scoop-software.de>
2016-02-22 09:27:02 +0100

    tipps mit info

--- app/zenmaster/data.js
+++ app/zenmaster/data.js
@@ -1,41 +1,47 @@
-var dataform = [
-    { type: "Form", type2: "Oberfläche", name: "ist lang und dünn oder stabförmig" },
-    { type: "Form", type2: "Oberfläche", name: "ist im wesentlichen flach" },
-    { type: "Form", type2: "Oberfläche", name: "ist kugel- oder eiförmig" },
-    { type: "Form", type2: "Oberfläche", name: "ist Kistenförmig oder rechteckig" },
-    { type: "Form", type2: "Oberfläche", name: "Oberfläche ist grösstenteils pelzig oder haarig" },
-    { type: "Form", type2: "Oberfläche", name: "Oberfläche ist warm oder heiss" },
-    { type: "Form", type2: "Oberfläche", name: "Oberfläche ist vorwiegend glatt" },
-    { type: "Form", type2: "Oberfläche", name: "Oberfläche ist vorwiegend rauh" },
-    { type: "Form", type2: "Oberfläche", name: "Oberfläche ist vorwiegend hart" },
-    { type: "Form", type2: "Oberfläche", name: "Oberfläche ist vorwiegend elastisch" },
-];
+var data =
+{
+    form: {type: "Form",type2: "Oberfläche",
+    cards:[
+    { name: "ist lang und dünn oder stabförmig" },
+    { name: "ist im wesentlichen flach" },
+    { name: "ist kugel- oder eiförmig" },
+    { name: "ist Kistenförmig oder rechteckig" },
+    { name: "Oberfläche ist grösstenteils pelzig oder haarig" },
+    { name: "Oberfläche ist warm oder heiss" },
+    { name: "Oberfläche ist vorwiegend glatt" },
+    { name: "Oberfläche ist vorwiegend rauh" },
+    { name: "Oberfläche ist vorwiegend hart" },
+    { name: "Oberfläche ist vorwiegend elastisch" },
+]},
 
-var datainhalt = [
-    { type: "Inhalt", type2: "Bewegung", name: "hat Zähne oder spitze Teile" },
-    { type: "Inhalt", type2: "Bewegung", name: "mit Rad oder vollständig drehbarem Teil" },
-    { type: "Inhalt", type2: "Bewegung", name: "mit Gelenk oder beweglichem Teil" },
-    { type: "Inhalt", type2: "Bewegung", name: "darauf sind Buchstaben, Zeichen oder Zahlen" },
-    { type: "Inhalt", type2: "Bewegung", name: "bewegt sich selbständig" },
-    { type: "Inhalt", type2: "Bewegung", name: "bewegt sich selbständig, aber eher langsam" },
-    { type: "Inhalt", type2: "Bewegung", name: "schwimmt im Wasser oben" },
-];
-var dataort = [
-    { type: "Ort", type2: "Zeit", name: "kann ich innerhalb von 5 Minuten hierherbringen" },
-    { type: "Ort", type2: "Zeit", name: "ist nicht im Umkreis von 100 Metern zu finden" },
-    { type: "Ort", type2: "Zeit", name: "gibt es eher im Wald als in der Stadt" },
-    { type: "Ort", type2: "Zeit", name: "gibt es häufiger in der Stadt als auf dem Land" },
-    { type: "Ort", type2: "Zeit", name: "wird eher im Winter als im Sommer verwendet" },
-    { type: "Ort", type2: "Zeit", name: "wird eher im Sommer als im Winter verwendet" },
-    { type: "Ort", type2: "Zeit", name: "gab es schon bei den alten Griechen oder Römern" },
-    { type: "Ort", type2: "Zeit", name: "es gibt jetzt mehr als vor 100 Jahren" },
-];
-var dataextra = [
-    { type2: "Größe", type: "Gewicht", name: "grösser als eine Katze" },
-    { type2: "Größe", type: "Gewicht", name: "passt in ein Gartenhäuschen" },
-    { type2: "Größe", type: "Gewicht", name: "höher als ein 2-stöckiges Reihenhaus" },
-    { type2: "Größe", type: "Gewicht", name: "passt in einen Schuhkarton" },
-    { type2: "Größe", type: "Gewicht", name: "ist 10kg oder leichter" },
-    { type2: "Größe", type: "Gewicht", name: "ist zwischen 10kg und 100kg" },
-    { type2: "Größe", type: "Gewicht", name: "ist 100kg oder schwerer" },
-];
+inhalt: {type: "Inhalt",type2: "Bewegung",
+cards:[
+    { name: "hat Zähne oder spitze Teile" },
+    { name: "mit Rad oder vollständig drehbarem Teil" },
+    { name: "mit Gelenk oder beweglichem Teil" },
+    { name: "darauf sind Buchstaben, Zeichen oder Zahlen" },
+    { name: "bewegt sich selbständig" },
+    { name: "bewegt sich selbständig, aber eher langsam" },
+    { name: "schwimmt im Wasser oben" },
+]},
+ort: {type: "Ort",type2: "Zeit",
+cards:[
+    {name: "kann ich innerhalb von 5 Minuten hierherbringen" },
+    {name: "ist nicht im Umkreis von 100 Metern zu finden" },
+    {name: "gibt es eher im Wald als in der Stadt" },
+    {name: "gibt es häufiger in der Stadt als auf dem Land" },
+    {name: "wird eher im Winter als im Sommer verwendet" },
+    {name: "wird eher im Sommer als im Winter verwendet" },
+    {name: "gab es schon bei den alten Griechen oder Römern" },
+    {name: "es gibt jetzt mehr als vor 100 Jahren" },
+]},
+extra: {type: "Größe",type2: "Gewicht",
+cards: [
+    { name: "grösser als eine Katze" },
+    { name: "passt in ein Gartenhäuschen" },
+    { name: "höher als ein 2-stöckiges Reihenhaus" },
+    { name: "passt in einen Schuhkarton" },
+    { name: "ist 10kg oder leichter" },
+    { name: "ist zwischen 10kg und 100kg" },
+    { name: "ist 100kg oder schwerer" },
+]}}
