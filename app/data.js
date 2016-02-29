7ea0cd9bddb4d900c02fdf39263bf576e91904d2
Matthias Schmitt <matthias.schmitt@scoop-software.de>
2016-02-29 13:22:52 +0100

    index

--- app/data.js
+++ app/data.js
@@ -1,8 +1,16 @@
-var games = [
-    {name: "ZenMaster", type: "Kommunikation", link:"zenmaster/index.html", pitch: " MasterMind mit Wörter", desc:"Finde ein Wort das vier spezifischen Kriterien genügt.", status:"test"},
-    {name: "MonsterMob", type: "Minimal", link:"monstermob/index.html", desc:"", status:"dev"},
-    {name: "CrimeScene", type: "Deduktion, Memory", link:"crimescene/index.html", desc:"", status:"test"},
-    {name: "Tiger", type: "SoloCardGame", link:"tiger/index.html", desc:"", status:"test"},
-    {name: "Green Hell", type: "SoloCardGame", link:"greenhell/index.html", desc:"", status:"test"},
-    {name: "Servietten", type: "Servietten Spiele", link:"servietten/index.html", desc:"", status:"dev"},
+var active = [
+    { name: "ZenMaster", type: "Kommunikation", link: "zenmaster/index.html", pitch: " MasterMind mit Wörter", desc: "Finde ein Wort das vier spezifischen Kriterien genügt.", status: "test" },
+    { name: "Servietten", type: "Servietten Spiele", link: "servietten/index.html", pitch: "", desc: "", status: "dev" },
+    { name: "MonsterMob", type: "Minimal", link: "monstermob/index.html", pitch: "", desc: "", status: "dev" },
+    { name: "Dorf Building", type: "Family", link: "", pitch: "Siedler - Dominion - Carcassone", desc: "", status: "dev" },
+    { name: "Green Hell", type: "SoloCardGame", link: "greenhell/index.html", pitch: "", desc: "", status: "test" },
+];
+var pipeline = [
+    { name: "CrimeScene", type: "Deduktion, Memory", link: "crimescene/index.html", desc: "", status: "test" },
+    { name: "Heisse Würfel", type: "cantStop", link: "hotdice/index.html", desc: "", status: "dev" },
+    { name: "Dracula", type: "SoloCardGame", link: "dracula/index.html", desc: "Neue Version des SoloCardGames mit Dracula", status: "concept" },
+    { name: "StadtChronik", type: "Partieübergreifend", link: "", desc: "Ergebnis der Partie wird in den folgenden Partien verwendet.", status: "concept" },
+];
+var archive = [
+    { name: "Tiger", type: "SoloCardGame", link: "tiger/index.html", desc: "Erste Version des SoloCardGames", status: "test" },
 ];
