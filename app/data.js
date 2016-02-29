82c74e9f8e0391970e89bcaf4c2ad3ba9e5deb3c
Matthias Schmitt <matthias.schmitt@scoop-software.de>
2016-02-29 12:55:36 +0100

    index

---
+++ app/data.js
@@ -0,0 +1,8 @@
+var games = [
+    {name: "ZenMaster", type: "Kommunikation", link:"zenmaster/index.html", pitch: " MasterMind mit Wörter", desc:"Finde ein Wort das vier spezifischen Kriterien genügt.", status:"test"},
+    {name: "MonsterMob", type: "Minimal", link:"monstermob/index.html", desc:"", status:"dev"},
+    {name: "CrimeScene", type: "Deduktion, Memory", link:"crimescene/index.html", desc:"", status:"test"},
+    {name: "Tiger", type: "SoloCardGame", link:"tiger/index.html", desc:"", status:"test"},
+    {name: "Green Hell", type: "SoloCardGame", link:"greenhell/index.html", desc:"", status:"test"},
+    {name: "Servietten", type: "Servietten Spiele", link:"servietten/index.html", desc:"", status:"dev"},
+];
