f45676f4c3ee5e25838ad2e9071f0faa114ce4fb
Matthias Schmitt <Matthias Schmitt>
2016-09-25 15:58:42 +0200

    way puzzle

--- app/waypuzzle/data.js
+++ app/waypuzzle/data.js
@@ -1,5 +1,31 @@
-var data = {
-    rows: [
+var data = [
+    {
+        name: "Regel 1: Bewegung",
+        rows:[
+       "         ",
+       " 3  2 1Z ",
+       "         ",
+            ],
+            rules: [
+                "pro Bewegung sinkt die Bewegungsweite um 1.",
+                "Bei Bewegungsweite 0 ist man tot."
+            ]
+    },
+    {
+        name: "Regel 2: Zielfeld",
+        rows:[
+       "     ",
+       " 5 Z ",
+       "     ",
+            ],
+            rules: [
+                "Das Zielfeld muss am Ende einer Bewegung erreicht werden.",
+                "Kein Feld darf mehrfach betreten werden."
+            ]
+    },
+    {
+        name: "Land",
+        rows:[
        "       ~~      ~~~Z",
        "      ~~ AD   ~~ ~ ",
        " DWW     AAAA~     ",
@@ -10,5 +36,11 @@ var data = {
        "~~~~~~~~~~WW       ",
        "                   ",
        "4   A  D  WWW      ",
+            ],
+            rules: [
+                "Bewegung pro Tag sinktdie Bwegung um 1.",
+                "Bei Bewegung 0 ist man tot."
             ]
-}
+        
+    }
+]
