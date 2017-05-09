9ddcdddecf4a47c4d91b33cc4b17f9886b6c7a97
Matthias Schmitt <mschmitt@db12.de>
2017-05-09 17:43:05 +0200

    mk images

--- app/mortalkombat/data.js
+++ app/mortalkombat/data.js
@@ -1,16 +1,25 @@
 var engine = {
     cards: [
         [
-        { upLeft: "A",upRight: "A", downLeft: "D" , downRight: "D", name: "MegaMan", hearts: 3, special: 3 },
-        { upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "MegaMan", hearts: 3, special: 3 },
-        { upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "MegaMan", hearts: 3, special: 3 },
-        { upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "MegaMan", hearts: 3, special: 3 },
+        { upLeft: "A",upRight: "X", downLeft: "X" , downRight: "D", name: "MegaMan", hearts: 6, special: 3 },
+        { upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Tank", hearts: 8, special: 1 },
+        { upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Heiler", hearts: 2, special: 7 },
+        { upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Fechter", hearts: 4, special: 5 },
         ],[
+        { back: "Right", upLeft: "X",upRight: "A", downLeft: "X" , downRight: "D", name: "MegaMan" },
+        { back: "Right", upLeft: "X",upRight: "A", downLeft: "D" , downRight: "D", name: "Tank" },
+        { back: "Right", upLeft: "X",upRight: "A", downLeft: "D" , downRight: "D", name: "Heiler" },
+        { back: "Right", upLeft: "X",upRight: "A", downLeft: "D" , downRight: "D", name: "Fechter" },
         ],[
-        { type: "Mob", subtype: "A", count: 4, dark: "dark", owner: "" },
-        { type: "Mob", subtype: "B", count: 4, dark: "dark", owner: "" },
-        { type: "Mob", subtype: "C", count: 4, dark: "dark", owner: "" },
-        { type: "Mob", subtype: "D", count: 4, dark: "dark", owner: "" },
+        { upLeft: "A",upRight: "A", downLeft: "D" , downRight: "D", name: "Magier", hearts: 3, special: 6 },
+        { upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Schütze", hearts: 5, special: 3 },
+        { upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Zyklop", hearts: 7, special: 2 },
+        { upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Zwerg", hearts: 6, special: 3 },
+        ],[
+        { back: "Right", upLeft: "A",upRight: "A", downLeft: "D" , downRight: "D", name: "Magier" },
+        { back: "Right", upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Schütze" },
+        { back: "Right", upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Zyklop" },
+        { back: "Right", upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Zwerg" },
         ]
     ],
     shuffle: function() {
