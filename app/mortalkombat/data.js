41a90686aad7f9c683a9c7b08444147d8c1a33a9
Matthias Schmitt <mschmitt@db12.de>
2017-05-10 10:18:46 +0200

    images

--- app/mortalkombat/data.js
+++ app/mortalkombat/data.js
@@ -1,32 +1,175 @@
 var engine = {
-    cards: [
-        [
-        { upLeft: "A",upRight: "X", downLeft: "X" , downRight: "D", name: "MegaMan", hearts: 6, special: 3 },
-        { upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Tank", hearts: 8, special: 1 },
-        { upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Heiler", hearts: 2, special: 7 },
-        { upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Fechter", hearts: 4, special: 5 },
-        ],[
-        { back: "Right", upLeft: "X",upRight: "A", downLeft: "X" , downRight: "D", name: "MegaMan" },
-        { back: "Right", upLeft: "X",upRight: "A", downLeft: "D" , downRight: "D", name: "Tank" },
-        { back: "Right", upLeft: "X",upRight: "A", downLeft: "D" , downRight: "D", name: "Heiler" },
-        { back: "Right", upLeft: "X",upRight: "A", downLeft: "D" , downRight: "D", name: "Fechter" },
-        ],[
-        { upLeft: "A",upRight: "A", downLeft: "D" , downRight: "D", name: "Magier", hearts: 3, special: 6 },
-        { upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Schütze", hearts: 5, special: 3 },
-        { upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Zyklop", hearts: 7, special: 2 },
-        { upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Zwerg", hearts: 6, special: 3 },
-        ],[
-        { back: "Right", upLeft: "A",upRight: "A", downLeft: "D" , downRight: "D", name: "Magier" },
-        { back: "Right", upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Schütze" },
-        { back: "Right", upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Zyklop" },
-        { back: "Right", upLeft: "A",upRight: "X", downLeft: "D" , downRight: "D", name: "Zwerg" },
-        ]
+  cards: [
+    [
+      {
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "KungFu",
+        desc: "",
+        hearts: 6,
+        special: 3
+      },
+      {
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "Tank",
+        desc: "",
+        hearts: 8,
+        special: 1
+      },
+      {
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "Heiler",
+        desc: "",
+        hearts: 2,
+        special: 7
+      },
+      {
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "Fechter",
+        desc: "",
+        hearts: 4,
+        special: 5
+      }
     ],
-    shuffle: function() {
-        for (var j, x, i = this.cards.length; i; j = Math.floor(Math.random() * i), x = this.cards[--i], this.cards[i] = this.cards[j], this.cards[j] = x);
-        return this.cards;
-    },
-    init: function(game) {
-
-    }
-}
+    [
+      {
+        back: "Right",
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "Tank",
+        desc: ""
+      },
+      {
+        back: "Right",
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "KungFu",
+        desc: ""
+      },
+      {
+        back: "Right",
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "Fechter",
+        desc: ""
+      },
+      {
+        back: "Right",
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "Heiler",
+        desc: ""
+      }
+    ],
+    [
+      {
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "Magier",
+        desc: "",
+        hearts: 3,
+        special: 6
+      },
+      {
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "Schütze",
+        desc: "",
+        hearts: 5,
+        special: 3
+      },
+      {
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "Zyklop",
+        desc: "",
+        hearts: 7,
+        special: 2
+      },
+      {
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "Zwerg",
+        desc: "",
+        hearts: 6,
+        special: 3
+      }
+    ],
+    [
+      {
+        back: "Right",
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "Schütze",
+        desc: ""
+      },
+      {
+        back: "Right",
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "Magier",
+        desc: ""
+      },
+      {
+        back: "Right",
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "Zwerg",
+        desc: ""
+      },
+      {
+        back: "Right",
+        upLeft: { typ: "A", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        upRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downLeft:{ typ: "X", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        downRight:{ typ: "D", effects: ["-1", "-2", "-1", "-1", "-1", "0"] },
+        name: "Zyklop",
+        desc: ""
+      }
+    ]
+  ],
+  shuffle: function() {
+    for (
+      var j, x, i = this.cards.length;
+      i;
+      (j = Math.floor(Math.random() * i)), (x = this.cards[--i]), (this.cards[
+        i
+      ] = this.cards[j]), (this.cards[j] = x)
+    );
+    return this.cards;
+  },
+  init: function(game) {}
+};
